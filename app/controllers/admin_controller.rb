f66576846a3d1b66c5b856bfc68c7148c12ad523
joe953 <joemazloum953@gmail.com>
2023-02-05 17:02:59 +0200

    user new edit delete login page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
     before_action :authenticate_user!
-    before_action :admin_check, only: %i[ dashboard english_teachers french_teachers recruiters materials ]
+    before_action :admin_check
 
     def dashboard
         @users = User.where(role: 0) 
@@ -26,12 +26,54 @@ class AdminController < ApplicationController
         @materials = Material.all
         render layout: "layouts/admin"
     end
+    
+    def new_user 
+        @user = User.new
+    end
 
+    def save_user
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User successfully created"
+            redirect_to admin_path
+        else
+            flash[:danger] = "Could not create user"
+            redirect_to admin_new_user_path
+        end
+    end
+    
+    def edit_user
+        set_user
+    end
+    
+    def update_user
+        set_user
+        if @user.update(user_params)
+            flash[:success] = "User successfully updated"
+            redirect_to admin_path
+        else
+            flash[:danger] = "Could not update user"
+            redirect_to admin_edit_user_path
+        end
+    end
+    
+    def delete_user
+        set_user
+        @user.destroy
+        flash[:success] = "User deleted"
+        redirect_to admin_path
+    end
+    
     protected
         def admin_check
             if current_user.role != 1
                 redirect_to root_path
             end
         end
-            
+        def set_user
+            @user = User.find(params[:id])
+        end
+        def user_params
+            params.require(:user).permit(:name, :email, :role, :image, :price, :description, :phone, :password, :password_confirmation)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,17 @@ Rails.application.routes.draw do
   
   get "test", to: "application#test"
 
-  get "/admin", to: "admin#dashboard", as: "dashboard"
+  get "/admin", to: "admin#dashboard", as: "admin"
   get "/admin/french", to: "admin#french_teachers", as: "admin_french_teachers"
   get "/admin/english", to: "admin#english_teachers", as: "admin_english_teachers"
   get "/admin/recruiters", to: "admin#recruiters", as: "admin_recruiters"
   get "/admin/materials", to: "admin#materials", as: "admin_materials"
+
+  get "/admin/user/new", to: "admin#new_user", as: "admin_new_user"
+  post "/admin/user/save", to: "admin#save_user", as: "admin_save_user"
+  get "/admin/user/:id", to: "admin#edit_user", as: "admin_edit_user"
+  post "/admin/user/:id", to: "admin#update_user", as: "admin_update_user"
+  delete "/admin/user/:id", to: "admin#delete_user", as: "admin_delete_user"
 
   root "application#index"
 end
